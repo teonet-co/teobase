616610c41717bf3830a577860604ed52b8962e2a
Anton Vasiliev <a_vasiliev@wargaming.net>
2019-11-08 20:28:58 +0300

    Added time functions for microseconds.

--- include/teobase/time.h
+++ include/teobase/time.h
@@ -9,6 +9,16 @@
 #include <stdint.h>
 
 /**
+ * Get current time in microseconds.
+ *
+ * @return current time in microseconds since Unix Epoch.
+ *
+ * @note On 32-bit linux systems return value is limited to maximum value of 32-bit signed integer.
+ */
+
+int64_t teotimeGetCurrentTimeUs();
+
+/**
  * Get current time in milliseconds.
  *
  * @return current time in milliseconds since Unix Epoch.
@@ -19,9 +29,21 @@
 int64_t teotimeGetCurrentTimeMs();
 
 /**
+ * Get time in microseconds between saved moment of time and current time.
+ *
+ * @param time_value Saved moment of time in microseconds.
+ *
+ * @return Time in microseconds between time_value and current time.
+ *
+ * @note Return value can be negative if time_value is in the future.
+ */
+
+int64_t teotimeGetTimePassedUs(int64_t time_value_us);
+
+/**
  * Get time in milliseconds between saved moment of time and current time.
  *
- * @param time_value Saved moment of time.
+ * @param time_value Saved moment of time in milliseconds.
  *
  * @return Time in milliseconds between time_value and current time.
  *
