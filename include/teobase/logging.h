510571f357228e1e6432bf6dfc75ec17be55e71b
Dmitry Chernikov <d_chernikov@wargaming.net>
2019-11-23 19:10:38 +0300

    Allow application to setup it's own custom log output function
    or disable it completely

--- include/teobase/logging.h
+++ include/teobase/logging.h
@@ -6,6 +6,21 @@
 #ifndef TEOBASE_LOGGING_H
 #define TEOBASE_LOGGING_H
 
+/**! Custom log output function declaration
+ * Can be used to override default logger
+ *  */
+typedef void (*teologOutputFunction_t)(const char* tag, const char* message);
+
+/**! Set current output function to @a logger.
+ * If @a logger is NULL - disables logging
+ *  */
+void set_log_output_function(teologOutputFunction_t logger);
+
+/**! Set current output function to default one
+ * thus, undoing effect of set_log_output_function
+ *  */
+void set_default_log_output(void);
+
 void log_debug(const char* tag, const char* message);
 void log_info(const char* tag, const char* message);
 void log_warning(const char* tag, const char* message);
