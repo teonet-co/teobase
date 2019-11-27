bcac8b9bdc18e60922da2fd6698a0dea48839a26
Dmitry Chernikov <d_chernikov@wargaming.net>
2019-11-27 05:24:22 +0300

    File, line, function name, severity in logging
    printf-like formatter support

--- include/teobase/logging.h
+++ include/teobase/logging.h
@@ -6,24 +6,63 @@
 #ifndef TEOBASE_LOGGING_H
 #define TEOBASE_LOGGING_H
 
-/**! Custom log output function declaration
- * Can be used to override default logger
- *  */
-typedef void (*teologOutputFunction_t)(const char* tag, const char* message);
+/**
+ * Message importance/verbocity type. Passed unmodified to output function.
+ * Default loggers always output all messages of types (ERROR, IMPORTANT, INFO)
+ * Debug builds additionally allows DEBUG messages.
+ * All types CUSTOM and above are skipped by default
+*/
+typedef enum TeoLogMessageType {
+  //! Error condition, possibly leaving program in inconsistent state.
+  TEOLOG_SEVERITY_ERROR = 0,
+  //! Significant message, like *_ERROR but leaves program consistent (or recoverable)
+  TEOLOG_SEVERITY_IMPORTANT = 1,
+  //! Ordinary message, states program execution stages
+  TEOLOG_SEVERITY_INFO = 2,
+  //! Extra verbosity, messages assisting in debugging, barely useful in normal execution
+  TEOLOG_SEVERITY_DEBUG = 3,
+  //! Application-defined types, intended for application-defined loggers
+  TEOLOG_SEVERITY_CUSTOM = 4,
+} TeoLogMessageType;
 
-/**! Set current output function to @a logger.
+/**
+ * Custom log output function declaration
+ * Can be used to override default logger
+*/
+typedef void (*teologOutputFunction_t)(const char *file, int line,
+                                       const char *func,
+                                       TeoLogMessageType type, const char *tag,
+                                       const char *message);
+
+/**
+ * Set current output function to @a logger.
  * If @a logger is NULL - disables logging
- *  */
+*/
 void set_log_output_function(teologOutputFunction_t logger);
 
-/**! Set current output function to default one
- * thus, undoing effect of set_log_output_function
- *  */
-void set_default_log_output(void);
+/**
+ * Default output function. Produce something like
+ * ./src/myFile.cpp:34899 ‘update‘>> [MyTagName|ERR] Kinda log example
+*/
+void teolog_output_default(const char *file, int line, const char *func,
+                         TeoLogMessageType type, const char *tag,
+                         const char *message);
+
+/**
+ * Compact output function. Produce something like
+ * [MyTagName|ERR] Kinda log example
+*/
+void teolog_output_compact(const char *file, int line, const char *func,
+                         TeoLogMessageType type, const char *tag,
+                         const char *message);
 
 void log_debug(const char* tag, const char* message);
 void log_info(const char* tag, const char* message);
-void log_warning(const char* tag, const char* message);
+void log_warning(const char* tag, const char* message); //< log_important alias
+void log_important(const char* tag, const char* message);
 void log_error(const char* tag, const char* message);
+
+void log_format(const char *file, int line, const char *func,
+                TeoLogMessageType type, const char *tag, const char *fmt, ...);
 
 #endif
--- src/teobase/logging.c
+++ src/teobase/logging.c
@@ -6,28 +6,125 @@
 #include <android/log.h>
 #elif defined(TEONET_OS_WINDOWS)
 #include "teobase/windows.h"
-#else
-#include <stdio.h>
 #endif
 
-static inline void default_log_message(const char* tag, const char* message) {
+#include <stdio.h>  // snprintf, vsnprintf, NULL, size_t
+#include <stdarg.h> // va_start, va_end, va_copy
+#include <stdlib.h> // malloc, free
+
+static inline const char *log_suffix(TeoLogMessageType value) {
+    switch (value) {
+    case TEOLOG_SEVERITY_ERROR: return ":ERR";
+    case TEOLOG_SEVERITY_IMPORTANT: return ":IMP";
+    case TEOLOG_SEVERITY_INFO: return ":INF";
+    case TEOLOG_SEVERITY_DEBUG: return ":DBG";
+    }
+    return "";
+}
+
 #if defined(TEONET_OS_ANDROID)
-    __android_log_print(ANDROID_LOG_ERROR, tag, "%s", message);
-#elif defined(TEONET_OS_WINDOWS)
-    OutputDebugStringA(message);
+static inline android_LogPriority log_prio(TeoLogMessageType value) {
+    switch (value) {
+    case TEOLOG_SEVERITY_ERROR: return ANDROID_LOG_ERROR;
+    case TEOLOG_SEVERITY_IMPORTANT: return ANDROID_LOG_WARN;
+    case TEOLOG_SEVERITY_INFO: return ANDROID_LOG_INFO;
+    case TEOLOG_SEVERITY_DEBUG: return ANDROID_LOG_DEBUG;
+    }
+    return ANDROID_LOG_VERBOSE;
+}
+#endif
+
+#ifdef DEBUG
+static const TeoLogMessageType outputLevel = TEOLOG_SEVERITY_DEBUG;
 #else
-    printf("[%s] %s\n", tag, message);
+static const TeoLogMessageType outputLevel = TEOLOG_SEVERITY_INFO;
+#endif
+
+void teolog_output_compact(const char * file, int line, const char * func,
+                         TeoLogMessageType type, const char *tag,
+                         const char *message) {
+    if (type > outputLevel) {
+        return; //  verbosity limit
+    }
+    if (message == NULL) { message = "<NULL>"; }
+    if (tag == NULL) { tag = ""; }
+
+    #if defined(TEONET_OS_ANDROID)
+    __android_log_print(log_prio(type), tag, "%s", message);
+    #elif defined(TEONET_OS_WINDOWS)
+    const char *suffix = log_suffix(type);
+    // Estimate buffer size
+    int len = snprintf(NULL, 0, "[%s%s] %s", tag, suffix, message);
+    if (len < 1) { return; }
+    char *buffer = malloc(len+1);
+    if (buffer == NULL) { return; }
+    snprintf(buffer, len+1, "[%s%s] %s", tag, suffix, message);
+    OutputDebugStringA(buffer);
+    free(buffer);
+    #else
+    const char *suffix = log_suffix(type);
+    printf("[%s%s] %s\n", tag, suffix, message);
+    #endif
+}
+
+void teolog_output_default(const char *file, int line, const char *func,
+                         TeoLogMessageType type, const char *tag,
+                         const char *message) {
+    if (type > outputLevel) {
+        return; //  verbosity limit
+    }
+    if (message == NULL) { message = "<NULL>"; }
+    if (tag == NULL) { tag = ""; }
+    if (file == NULL) { file = "??"; }
+    if (func == NULL) { func = "??"; }
+
+   #if defined(TEONET_OS_ANDROID)
+    __android_log_print(log_prio(type), tag, "%s:%d ‘%s‘>> %s", file, (int)line,
+                        func, message);
+    #elif defined(TEONET_OS_WINDOWS)
+    const char *suffix = log_suffix(type);
+    // Estimate buffer size
+    size_t len = snprintf(NULL, 0, "%s:%d ‘%s‘>> [%s%s] %s", file, (int)line,
+                          func, tag, suffix, message);
+    if (len < 1) { return; }
+    char *buffer = malloc(len+1);
+    if (buffer == NULL) { return; }
+    snprintf(buffer, len+1, "%s:%d ‘%s‘>> [%s%s] %s", file, (int)line,
+             func, tag, suffix, message);
+    OutputDebugStringA(buffer);
+    free(buffer);
+#else
+    const char *suffix = log_suffix(type);
+    printf("%s:%d ‘%s‘>> [%s%s] %s\n", file, (int)line, func, tag, suffix, message);
 #endif
 }
 
-static teologOutputFunction_t log_message = default_log_message;
+static teologOutputFunction_t log_message = teolog_output_default;
 
 void set_log_output_function(teologOutputFunction_t logger) {
     log_message = logger;
 }
 
-void set_default_log_output(void) {
-    log_message = default_log_message;
+void log_format(const char *file, int line, const char *func,
+                TeoLogMessageType type, const char *tag, const char *fmt, ...) {
+    if (log_message == NULL) { return; }
+
+    va_list args_noop;
+    va_start(args_noop, fmt);
+    int message_len = vsnprintf(NULL, 0, fmt, args_noop);
+    va_end(args_noop);
+
+    if (message_len < 1) { return; }
+    char *message = malloc(message_len+1);
+    if (message == NULL) { return; }
+
+    va_list args_fmt;
+    va_start(args_fmt, fmt);
+    vsnprintf(message, message_len+1, fmt, args_fmt);
+    va_end(args_fmt);
+
+    log_message(file, line, func, type, tag, message);
+    free(message);
 }
 
 void log_debug(const char *tag, const char *message) {
@@ -35,7 +132,7 @@ void log_debug(const char *tag, const char *message) {
         return;
     }
 
-    log_message(tag, message);
+    log_message(NULL, -1, NULL, TEOLOG_SEVERITY_DEBUG, tag, message);
 }
 
 void log_info(const char* tag, const char* message) {
@@ -43,7 +140,7 @@ void log_info(const char* tag, const char* message) {
         return;
     }
 
-    log_message(tag, message);
+    log_message(NULL, -1, NULL, TEOLOG_SEVERITY_INFO, tag, message);
 }
 
 void log_warning(const char* tag, const char* message) {
@@ -51,7 +148,15 @@ void log_warning(const char* tag, const char* message) {
         return;
     }
 
-    log_message(tag, message);
+    log_message(NULL, -1, NULL, TEOLOG_SEVERITY_IMPORTANT, tag, message);
+}
+
+void log_important(const char* tag, const char* message){
+    if (log_message == NULL) {
+        return;
+    }
+
+    log_message(NULL, -1, NULL, TEOLOG_SEVERITY_IMPORTANT, tag, message);
 }
 
 void log_error(const char* tag, const char* message) {
@@ -59,5 +164,6 @@ void log_error(const char* tag, const char* message) {
         return;
     }
 
-    log_message(tag, message);
+    log_message(NULL, -1, NULL, TEOLOG_SEVERITY_ERROR, tag, message);
 }
+
