9dfa36cd102897d8e39bee207374c8b07ea0d517
Anton Vasiliev <si1ver1502@gmail.com>
2019-12-18 19:52:21 +0300

    Add cross-platform mutex functions.

---
+++ src/teobase/mutex.c
@@ -0,0 +1,91 @@
+#include "teobase/mutex.h"
+
+#include "teobase/platform.h"
+
+#include <stdbool.h>
+
+#if defined(TEONET_OS_WINDOWS)
+#include "teobase/windows.h"
+#else
+#include <errno.h>
+#include <pthread.h>
+#include <stdlib.h>
+#endif
+
+#include "teobase/logging.h"
+
+// Initialize mutex object.
+void teomutexInitialize(teonetMutex* mutex) {
+#if defined(TEONET_OS_WINDOWS)
+    InitializeCriticalSection(&mutex->critical_section);
+#else
+    int init_result = pthread_mutex_init(&mutex->mutex, NULL);
+
+    if (init_result != 0) {
+        LTRACK_E("TeoBase", "Failed to initialize mutex. Error code: %d.", init_result);
+        abort();
+    }
+#endif
+}
+
+// Locks mutex object.
+void teomutexLock(teonetMutex* mutex) {
+#if defined(TEONET_OS_WINDOWS)
+    EnterCriticalSection(&mutex->critical_section);
+#else
+    int lock_result = pthread_mutex_lock(&mutex->mutex);
+
+    if (lock_result != 0) {
+        LTRACK_E("TeoBase", "Failed to lock mutex. Error code: %d.", lock_result);
+        abort();
+    }
+#endif
+}
+
+// Tries to lock mutex object.
+bool teomutexTryLock(teonetMutex* mutex) {
+#if defined(TEONET_OS_WINDOWS)
+    BOOL try_lock_result = TryEnterCriticalSection(&mutex->critical_section);
+
+    return try_lock_result != 0 ? true : false;
+#else
+    int try_lock_result = pthread_mutex_trylock(&mutex->mutex);
+
+    if (try_lock_result == 0) {
+        return true;
+    } else if (try_lock_result == EBUSY) {
+        return false;
+    } else {
+        LTRACK_E("TeoBase", "Error while trying to lock mutex. Error code: %d.", try_lock_result);
+        abort();
+    }
+#endif
+}
+
+// Unlocks locked mutex object.
+void teomutexUnlock(teonetMutex* mutex) {
+#if defined(TEONET_OS_WINDOWS)
+    LeaveCriticalSection(&mutex->critical_section);
+#else
+    int unlock_result = pthread_mutex_unlock(&mutex->mutex);
+
+    if (unlock_result != 0) {
+        LTRACK_E("TeoBase", "Failed to unlock mutex. Error code: %d.", unlock_result);
+        abort();
+    }
+#endif
+}
+
+// Destroys mutex object.
+void teomutexDestroy(teonetMutex* mutex) {
+#if defined(TEONET_OS_WINDOWS)
+    DeleteCriticalSection(&mutex->critical_section);
+#else
+    int destroy_result = pthread_mutex_destroy(&mutex->mutex);
+
+    if (destroy_result != 0) {
+        LTRACK_E("TeoBase", "Failed to destroy mutex. Error code: %d.", destroy_result);
+        abort();
+    }
+#endif
+}
